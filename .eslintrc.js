d34d550ef3c6225f4c311d4908c5f945d704147d
hw <1968759808@qq.com>
2022-10-13 23:13:34 +0800

    config🔧:  修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
 		es2021: true,
 		node: true
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	// "plugin:@typescript-eslint/recommended"
+	extends: ["eslint:recommended"],
 	overrides: [],
 	parser: "@typescript-eslint/parser",
 	parserOptions: {
@@ -15,8 +16,6 @@ module.exports = {
 	rules: {
 		indent: ["error", "tab"],
 		"linebreak-style": ["error", "windows"],
-		quotes: ["error", "double"],
-		semi: ["error", "always"],
-		"@typescript-eslint/no-explicit-any": "off"
+		semi: ["error", "always"]
 	}
 };
